60b46b722c7c27794f984c8a74a2bd8a3d37636d
cti23gy <70219724+cti23gy@users.noreply.github.com>
2020-09-01 19:46:29 -0400

    Kirby

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,16 +1,44 @@
 function setup() {
     var green = "#87FF87"
+    var pink1 = "#ffa6c9"
+    var pink2 = "#db4f90"
+    var pink3 = "#db4fac"
+    var black1 = "#060204"
+    var white1 = "#FAE7FC"
     createCanvas(800, 600);
     
-    background(237, 210, 195);
+    //back layer
+    rotate(PI / 3.0);    //Drawing Method 1
+    fill(pink1);
+    ellipse(340, -250, 120, 135);    //Drawing Method 2
+    ellipse(230, 0, 100, 135);
+    rotate(-(PI / 3.0)*(PI / 3.0));
 
-    fill(200, 90, 80);
-    //fill(green);
-    circle(100, 100, 50);
-    stroke(217, 135, 237);
-    strokeWeight(10);
-    //noStroke();
-    rect(300, 200, 60, 30);
-    stroke(3);
-    line(20, 200, 300, 20);
+    fill(pink2);
+    ellipse(350, 350, 180, 100);
+    
+    //body
+    fill(pink1);
+    circle(250, 250, 150);    //Drawing Method 3
+
+    //front layer
+    rotate(PI / 3.0);
+    fill(pink2);
+    ellipse(350, 70, 155, 105);
+    rotate(-(PI / 3.0)*(PI / 3.0));
+
+    fill(black1);
+    ellipse(200, 200, 20, 50);
+    ellipse(280, 200, 20, 50);
+    noStroke();    //Drawing Method 4
+    fill(white1);
+    ellipse(200, 191, 10, 30);
+    ellipse(280, 191, 10, 30);
+    fill(pink3);
+    ellipse(150, 250, 40, 20);
+    ellipse(330, 250, 40, 20);
+    fill(black1);
+    arc(240, 265, 38, 60, 2*PI, PI);    //Drawing Method 5
+    fill(pink3);
+    
 }
