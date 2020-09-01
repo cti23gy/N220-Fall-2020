5f66166516a9e19c6c2e4896dc5c915609c4f643
cti23gy <70219724+cti23gy@users.noreply.github.com>
2020-09-01 17:19:48 -0400

    assignment1

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,16 @@
+function setup() {
+    var green = "#87FF87"
+    createCanvas(800, 600);
+    
+    background(237, 210, 195);
+
+    fill(200, 90, 80);
+    //fill(green);
+    circle(100, 100, 50);
+    stroke(217, 135, 237);
+    strokeWeight(10);
+    //noStroke();
+    rect(300, 200, 60, 30);
+    stroke(3);
+    line(20, 200, 300, 20);
+}
